9e48fd69e8a7267c9dfcd3a77bca31d7aa088795
Max <maxakipust@gmail.com>
2020-03-09 00:34:52 -0400

    Initial commit (up to chapter 18)

---
+++ src/compiler.h
@@ -0,0 +1,12 @@
+//
+// Created by max on 3/3/20.
+//
+
+#ifndef CLOX_COMPILER_H
+#define CLOX_COMPILER_H
+
+#include "vm.h"
+
+bool compile(const char* source, Chunk* chunk);
+
+#endif //CLOX_COMPILER_H
