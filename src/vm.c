8b2d43cfd85421fda4a1856d1d1e0ec9c3f02d75
Max <maxakipust@gmail.com>
2020-03-10 00:26:14 -0400

    implemented global variables (finished chapter 21)

--- src/vm.c
+++ src/vm.c
@@ -16,11 +16,11 @@
 
 VM vm;
 
-static void resetStack(){
+static void resetStack() {
     vm.stackTop = vm.stack;
 }
 
-static void runtimeError(const char* format, ...) {
+static void runtimeError(const char *format, ...) {
     va_list args;
     va_start(args, format);
     vfprintf(stderr, format, args);
@@ -34,13 +34,15 @@ static void runtimeError(const char* format, ...) {
     resetStack();
 }
 
-void initVM(){
+void initVM() {
     resetStack();
     vm.objects = NULL;
+    initTable(&vm.globals);
     initTable(&vm.strings);
 }
 
-void freeVM(){
+void freeVM() {
+    freeTable(&vm.globals);
     freeTable(&vm.strings);
     freeObjects();
 }
@@ -59,27 +61,28 @@ static Value peek(int distance) {
     return vm.stackTop[-1 - distance];
 }
 
-static bool isFalsey(Value value){
+static bool isFalsey(Value value) {
     return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
 }
 
-static void concatenate(){
-    ObjString* b = AS_STRING(pop());
-    ObjString* a = AS_STRING(pop());
+static void concatenate() {
+    ObjString *b = AS_STRING(pop());
+    ObjString *a = AS_STRING(pop());
 
     int length = a->length + b->length;
-    char* chars = ALLOCATE(char, length+1);
+    char *chars = ALLOCATE(char, length + 1);
     memcpy(chars, a->chars, a->length);
     memcpy(chars + a->length, b->chars, b->length);
     chars[length] = '\0';
 
-    ObjString* result = takeString(chars, length);
+    ObjString *result = takeString(chars, length);
     push(OBJ_VAL(result));
 }
 
-static InterpretResult run(){
+static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
-#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()]);
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_STRING() AS_STRING(READ_CONSTANT())
 
 #define BINARY_OP(valueType, op) \
     do {\
@@ -92,93 +95,126 @@ static InterpretResult run(){
         push(valueType(a op b)); \
     } while (false)
 
-    while(true){
+    while (true) {
 #ifdef DEBUG_TRACE_EXECUTION
         printf("          ");
-        for (Value* slot = vm.stack; slot < vm.stackTop; slot++){
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
             printf("[ ");
             printValue(*slot);
             printf(" ]");
         }
         printf("\n");
 
-        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+        disassembleInstruction(vm.chunk, (int) (vm.ip - vm.chunk->code));
 #endif
 
         uint8_t instruction;
-        switch (instruction = READ_BYTE())
+        switch (instruction = READ_BYTE()) {
             case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
                 push(constant);
                 break;
-            case OP_NIL:
-                push(NIL_VAL);
+                case OP_NIL:
+                    push(NIL_VAL);
                 break;
-            case OP_TRUE:
-                push(BOOL_VAL(true));
+                case OP_TRUE:
+                    push(BOOL_VAL(true));
                 break;
-            case OP_FALSE:
-                push(BOOL_VAL(false));
+                case OP_FALSE:
+                    push(BOOL_VAL(false));
                 break;
-            case OP_EQUAL: {
-                Value b = pop();
-                Value a = pop();
-                push(BOOL_VAL(valuesEqual(a,b)));
-                break;
-            }
-            case OP_GREATER:
-                BINARY_OP(BOOL_VAL, >);
-                break;
-            case OP_LESS:
-                BINARY_OP(BOOL_VAL, <);
-                break;
-            case OP_ADD:
-                if(IS_STRING(peek(0)) && IS_STRING(peek(1))){
-                    concatenate();
-                }else if(IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))){
-                    double b = AS_NUMBER(pop());
-                    double a = AS_NUMBER(pop());
-                    push(NUMBER_VAL(a+b));
-                }else{
-                    runtimeError("Operands bust be two numbers or two strings.");
-                    return INTERPRET_RUNTIME_ERROR;
+                case OP_EQUAL: {
+                    Value b = pop();
+                    Value a = pop();
+                    push(BOOL_VAL(valuesEqual(a, b)));
+                    break;
                 }
+                case OP_POP:
+                    pop();
                 break;
-            case OP_SUBTRACT:
-                BINARY_OP(NUMBER_VAL, -);
-                break;
-            case OP_MULTIPLY:
-                BINARY_OP(NUMBER_VAL, *);
-                break;
-            case OP_DIVIDE:
-                BINARY_OP(NUMBER_VAL, /);
-                break;
-            case OP_NOT:
-                push(BOOL_VAL(isFalsey(pop())));
-                break;
-            case OP_NEGATE:
-                if(!IS_NUMBER(peek(0))) {
-                    runtimeError("Operand must be a number.");
-                    return INTERPRET_RUNTIME_ERROR;
+                case OP_GET_GLOBAL: {
+                    ObjString *name = READ_STRING();
+                    Value value;
+                    if (!tableGet(&vm.globals, name, &value)) {
+                        runtimeError("Undefined variable '%s'.", name->chars);
+                        return INTERPRET_RUNTIME_ERROR;
+                    }
+                    push(value);
+                    break;
                 }
+                case OP_DEFINE_GLOBAL: {
+                    ObjString *name = READ_STRING();
+                    tableSet(&vm.globals, name, peek(0));
+                    pop();
+                    break;
+                }
+                case OP_SET_GLOBAL: {
+                    ObjString* name = READ_STRING();
+                    if(tableSet(&vm.globals, name, peek(0))){
+                        tableDelete(&vm.globals, name);
+                        runtimeError("Undefined variable '%s'.", name->chars);
+                        return INTERPRET_RUNTIME_ERROR;
+                    }
+                    break;
+                }
+
+                case OP_GREATER:
+                    BINARY_OP(BOOL_VAL, >);
+                break;
+                case OP_LESS:
+                    BINARY_OP(BOOL_VAL, <);
+                break;
+                case OP_ADD:
+                    if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
+                        concatenate();
+                    } else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
+                        double b = AS_NUMBER(pop());
+                        double a = AS_NUMBER(pop());
+                        push(NUMBER_VAL(a + b));
+                    } else {
+                        runtimeError("Operands bust be two numbers or two strings.");
+                        return INTERPRET_RUNTIME_ERROR;
+                    }
+                break;
+                case OP_SUBTRACT:
+                    BINARY_OP(NUMBER_VAL, -);
+                break;
+                case OP_MULTIPLY:
+                    BINARY_OP(NUMBER_VAL, *);
+                break;
+                case OP_DIVIDE:
+                    BINARY_OP(NUMBER_VAL, /);
+                break;
+                case OP_NOT:
+                    push(BOOL_VAL(isFalsey(pop())));
+                break;
+                case OP_NEGATE:
+                    if (!IS_NUMBER(peek(0))) {
+                        runtimeError("Operand must be a number.");
+                        return INTERPRET_RUNTIME_ERROR;
+                    }
                 push(NUMBER_VAL(-AS_NUMBER(pop())));
                 break;
-            case OP_RETURN:
-                printValue(pop());
+                case OP_PRINT:
+                    printValue(pop());
                 printf("\n");
-                return INTERPRET_OK;
+                break;
+                case OP_RETURN:
+                    return INTERPRET_OK;
+            }
         }
     }
 #undef BINARY_OP
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef READ_STRING
 }
 
-InterpretResult interpret(const char* source){
+InterpretResult interpret(const char *source) {
     Chunk chunk;
     initChunk(&chunk);
 
-    if(!compile(source, &chunk)){
+    if (!compile(source, &chunk)) {
         freeChunk(&chunk);
         return INTERPRET_COMPILE_ERROR;
     }
