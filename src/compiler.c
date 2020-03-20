ce04508924e96f20ac64ebbb62679981a123b653
Max <maxakipust@gmail.com>
2020-03-19 21:55:25 -0400

    part way through a chapter

--- src/compiler.c
+++ src/compiler.c
@@ -269,6 +269,26 @@ static int resolveLocal(Compiler* compiler, Token* name) {
     return -1;
 }
 
+static int addUpvalue(Compiler* compiler, uint8_t index, bool isLocal){
+    int upvalueCount = compiler->function->upvalueCount;
+    compiler->upvalues[upvalueCount].isLocal = isLocal;
+    compiler->upvalues[upvalueCount].index = index;
+    return compiler->function->upvalueCount++;
+}
+
+static int resolveUpvalue(Compiler* compiler, Token* name){
+    if(compiler->enclosing == NULL){
+        return -1;
+    }
+
+    int local = resolveLocal(compiler->enclosing, name);
+    if(local != -1){
+        return addUpvalue(compiler, (uint8_t)local, true);
+    }
+
+    return -1;
+}
+
 static void addLocal(Token name) {
     if(current->localCount == UINT8_COUNT){
         error("Too many local variables in function.");
@@ -389,7 +409,7 @@ static void function(FunctionType type){
     block();
 
     ObjFunction* function = endCompiler();
-    emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+    emitBytes(OP_CLOSURE, makeConstant(OBJ_VAL(function)));
 }
 
 static void funDeclaration(){
@@ -706,9 +726,12 @@ static void string(bool canAssign) {
 static void namedVariable(Token name, bool canAssign){
     uint8_t getOp, setOp;
     int arg = resolveLocal(current, &name);
-    if(arg != -1){
+    if(arg != -1) {
         getOp = OP_GET_LOCAL;
         setOp = OP_SET_LOCAL;
+    }else if((arg = resolveUpvalue(current, &name)) != -1){
+        getOp = OP_GET_UPVALUE;
+        setOp = OP_SET_UPVALUE;
     }else{
         arg = identifierConstant(&name);
         getOp = OP_GET_GLOBAL;
