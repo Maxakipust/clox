d16d93d4d64c59b444faae9db3de609770e7e6f4
Max <maxakipust@gmail.com>
2020-03-21 17:31:58 -0400

    finished GC (chapter 26)

--- src/memory.h
+++ src/memory.h
@@ -23,6 +23,9 @@
 #define FREE_ARRAY(type, pointer, oldCount) reallocate(pointer, sizeof(type) * (oldCount) ,0)
 
 void* reallocate(void* previous, size_t oldSize, size_t newSize);
+void markObject(Obj* object);
+void markValue(Value value);
+void collectGarbage();
 void freeObjects();
 
 #endif //CLOX_MEMORY_H
